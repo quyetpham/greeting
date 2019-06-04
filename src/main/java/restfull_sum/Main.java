4dcd075c3f61b86869c3a45f956d214e669cab3f
quyetpv2 <quyetpham310@gmail.com>
2019-06-04 17:20:54 +0700

    init

---
+++ src/main/java/restfull_sum/Main.java
@@ -0,0 +1,16 @@
+package restfull_sum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class Main {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello word");
+		SpringApplication.run(Main.class, args);
+	}
+
+}
